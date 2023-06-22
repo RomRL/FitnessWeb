09960ed80efe704d3d5eb4e0b0b3fff91baa033f
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2023-06-23 01:24:13 +0300

    fix highr model

--- client/src/componenets/General/HighetModal.jsx
+++ client/src/componenets/General/HighetModal.jsx
@@ -10,6 +10,8 @@ import {
   MDBContainer,
   MDBValidation,
   MDBValidationItem,
+  MDBCol,
+  MDBRow,
 } from 'mdb-react-ui-kit';
 import Modal from 'react-bootstrap/Modal';
 
@@ -18,16 +20,13 @@ export default function HeightModal({ setHeight }) {
   const [editHeight, setEditHeight] = useState('');
 
   const changeHeight = () => {
-    if (editHeight >= 0 && editHeight <= 210) {
+    if (editHeight >= 50 && editHeight <= 210) {
       const response = updateHeight(editHeight);
       if (response === false) {
-        alert('Error');
       } else {
         setHeight(editHeight);
         setShowModal(false);
       }
-    } else {
-      alert('Please choose a height between 40 and 210.');
     }
   };
 
@@ -41,7 +40,7 @@ export default function HeightModal({ setHeight }) {
         Edit Height
       </MDBBtn>
 
-      <Modal show={showModal} tabIndex="-1">
+      <Modal show={showModal} tabIndex="-1" >
         <MDBModalHeader className="bg-dark text-white">
           <MDBModalTitle>Set Height</MDBModalTitle>
           <MDBBtn
@@ -50,46 +49,45 @@ export default function HeightModal({ setHeight }) {
             onClick={toggleModal}
           ></MDBBtn>
         </MDBModalHeader>
-        <MDBContainer onSubmit={changeHeight} >
+        <MDBModalBody className="d-flex justify-content-center" onSubmit={changeHeight} >
+          <MDBValidation className="row g-3">
+            <MDBRow className="d-flex justify-content-center g-2 py-2 fw-bold">
+              Insert your new height
+            </MDBRow>
+            <MDBValidationItem feedback='Min 50 cm Max 250 cm ' invalid  >
+              <MDBRow className="justify-content-center g-2 py-2">
+                <MDBCol md="5">
+                  <MDBInput
+                    value={editHeight}
+                    name="Height"
+                    onChange={(event) => setEditHeight(event.target.value)}
+                    id="Height"
+                    required
+                    label="Height"
+                    type='number'
+                    min={50}
+                    max={210}
+                    size="lg"
+                    labelClass="text-black"
+                    style={{ color: 'black' }}
+                  />
+                </MDBCol>
+              </MDBRow>
 
-        <MDBModalBody>
-            <MDBValidation className="row g-3"  >
-                <div className="col-9">
-                  <p>Insert your new height</p>
-                </div>
-                <MDBValidationItem feedback='Enter Weight (Min 40 kg Max 250 kg )' invalid className="col-md-4">
-                <MDBInput
-                  value={editHeight}
-                  name="Height"
-                  onChange={(event) => setEditHeight(event.target.value)}
-                  id="Height"
-                  required
-                  label="Height"
-                  type="number"
-                  min={50}
-                  max={210}
-                  size="lg"
-                  wrapperClass="mb-4"
-                  labelClass="text-black"
-                  style={{ color: 'black' }}
-                />
-              </MDBValidationItem>
-            </MDBValidation>
-        </MDBModalBody>
+            </MDBValidationItem>
+            <hr />
 
+            <MDBRow className="justify-content-center ">
+              <MDBCol md="4">
+                <MDBBtn color="success" type='submit' >
+                  Change Height
+                </MDBBtn>
+              </MDBCol>
+            </MDBRow>
+        </MDBValidation>
+      </MDBModalBody>
 
-        <MDBModalFooter>
-          <MDBBtn color="success" type='submit'>
-            Change Height
-          </MDBBtn>
-          <MDBBtn color="danger" onClick={toggleModal}>
-            Close
-          </MDBBtn>
-          
-        </MDBModalFooter>
-        </MDBContainer>
-
-      </Modal>
+    </Modal >
     </>
   );
 }
