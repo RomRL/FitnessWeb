6d0c6a4ddb22a68afb5ab626162da47cac525af2
RomRL <romharel98@gmail.com>
2023-06-22 19:37:31 +0300

    ExpertCard

--- client/src/componenets/UserPageComp/ExpertCard.jsx
+++ client/src/componenets/UserPageComp/ExpertCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ExpertTable from "./ExpertTable";
-import { MDBCard, MDBCol,MDBCardBody } from "mdb-react-ui-kit";
+import { MDBCard, MDBCol, MDBCardBody } from "mdb-react-ui-kit";
 export default function ExpertCard(props) {
   const { data } = props;
   //  Format the string to be displayed in the card
-  
+
   const averageWeightLossPerDay = Object.entries(data.averageWeightLossPerDay).map(([key, value]) => {
     if (value > 0)
       return (`$${key}  You gained ${value.toFixed(1)} kg$`);
@@ -14,25 +14,17 @@ export default function ExpertCard(props) {
   }).join(" ");
 
   return (
-  
-    <MDBCol >
-    <MDBCard >
-      <MDBCardBody>
-          <ExpertTable data={data} />
-    </MDBCardBody>
-    </MDBCard>
-  </MDBCol>
-    // <BigCard title="General information"
-    // text={
-    //       // Cheack if data.averageWeightLossPerDay is empty
-    //    (data.min === "" &&  data.max === "")
-    //       ? "You need to work one more time to see the data"
-    //       : `#Average Weight Loss Per Day $${averageWeightLossPerDay}$ #   #Gained The Most Weight $${data.min} #Lost The Most Weight $${data.max}$`
-    //   }
-    //   img_src={getURL("general-info")}>
-        
 
-    //      </BigCard>
+    <MDBCol >
+      <MDBCard >
+        <MDBCardBody>
+          <div className="table-responsive">
+            <ExpertTable data={data} />
+          </div>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBCol>
+
 
   );
 }
