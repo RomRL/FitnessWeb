5b26ae9e81256ba0cae3fc520bfad5b94fc01361
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2023-06-22 22:18:37 +0300

    before change login

--- client/src/componenets/AuthPageComp/LoginComponent.jsx
+++ client/src/componenets/AuthPageComp/LoginComponent.jsx
@@ -59,11 +59,8 @@ function LoginComponent() {
   return (
     <MainLayout >
       <MDBContainer fluid onSubmit={handleSubmit}>
-        {/* <div className="p-5 bg-image" style={{ backgroundImage: 'url(https://mdbootstrap.com/img/new/textures/full/171.jpg)', borderRadius: '10px', margin: '2vh', height: '300px' }}></div> */}
-        {/* <div style={{  backgroundImage: `url(${background})`, borderRadius: '10px' }} > */}
-
         <MDBRow className="d-flex justify-content-center align-items-center ">
-          <MDBCol col="12">
+          <MDBCol >
             <br />
             <MDBCard
               className="bg-dark text-white mx-auto mb-10 "
--- client/src/componenets/AuthPageComp/LoginForm.jsx
+++ client/src/componenets/AuthPageComp/LoginForm.jsx
@@ -12,55 +12,55 @@ import {
 function LoginForm({ email, password, handleEmailChange, handlePasswordChange, handleSubmit }) {
   return (
 
-      <MDBValidation className='row g-3 align-items-center' >
-          <MDBValidationItem feedback='Please choose an Email.' invalid >
-            <MDBInput
-              value={email}
-              name='email'
-              onChange={handleEmailChange}
-              id='email'
-              required
-              label='Email address'
-              type='email'
-              size="lg"
-              labelClass='text-white'
-              style={{ color: 'white' }}
-            />
-          </MDBValidationItem>
+    <MDBValidation className='row g-3 align-items-center' >
+      <MDBValidationItem feedback='Please choose an Email.' invalid >
+        <MDBInput
+          value={email}
+          name='email'
+          onChange={handleEmailChange}
+          id='email'
+          required
+          label='Email address'
+          type='email'
+          size="lg"
+          labelClass='text-white'
+          style={{ color: 'white' }}
+        />
+      </MDBValidationItem>
 
-          <MDBValidationItem feedback='Please choose a Password.' invalid >
-            <MDBInput
-              value={password}
-              name='password'
-              onChange={handlePasswordChange}
-              id='password'
-              required
-              label='Password'
-              type='password'
-              size="lg"
-              labelClass='text-white'
-              inputClass='text-white'
-              style={{ color: 'white' }}
-            />
-          </MDBValidationItem>
+      <MDBValidationItem feedback='Please choose a Password.' invalid >
+        <MDBInput
+          value={password}
+          name='password'
+          onChange={handlePasswordChange}
+          id='password'
+          required
+          label='Password'
+          type='password'
+          size="lg"
+          labelClass='text-white'
+          inputClass='text-white'
+          style={{ color: 'white' }}
+        />
+      </MDBValidationItem>
 
-        <div className="d-flex justify-content-center">
-          <MDBBtn
-            outline
-            className='mx-2 px-5'
-            color='white'
-            size='lg'
-            type='submit'
-            inputClass='text-white'
-            data-mdb-toggle="modal"
-            data-mdb-target="#exampleModal"
-            style={{ margin: '2vh' }}
-            onClick={handleSubmit}
-          >
-            Login
-          </MDBBtn>
-        </div>
-      </MDBValidation>
+      <div className="d-flex justify-content-center">
+        <MDBBtn
+          outline
+          className='mx-2 px-5'
+          color='white'
+          size='lg'
+          type='submit'
+          inputClass='text-white'
+          data-mdb-toggle="modal"
+          data-mdb-target="#exampleModal"
+          style={{ margin: '2vh' }}
+          onClick={handleSubmit}
+        >
+          Login
+        </MDBBtn>
+      </div>
+    </MDBValidation>
 
   );
 }
