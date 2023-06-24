646f8208b3e86b01d845c493e0980a85fc1c7a3f
RomRL <romharel98@gmail.com>
2023-06-24 23:46:56 +0300

    Merged

---
+++ client/src/functionalRequirments.js
@@ -0,0 +1,53 @@
+Functional Requirments :
+* The system will be splited to client and server.
+* The system will be database .
+* The system will be web application.
+* The system will fit also to mobile application.
+* The system will deployed .
+
+=====Users Requirments=====
+* The system will allow the user to create a new account.
+* The system will allow the user to login to their account.
+* The system will allow the user to logout of their account.
+* The system will allow the user to view their account information.
+* The system will allow the user to edit height in their account.
+* The system will allow the user to edit weight in their account.
+* The system will provide secure login and logout functionality.
+* The system will save only vaild information to the database.
+* The system will allow navigation between pages.
+* The system will saving information to the database.
+* The system will allow user to log in only from one device at a time.
+* The system will autumaticly log out the user after 20 minutes from login
+
+=====Muscle Group Requirments=====
+* The system will allow the user to learn about the muscle groups.
+* The system will allow the user to learn about the exercises for each muscle group.
+* The system will show video examples of each exercise.
+* The system will allow the user to learn more from external sources.
+* The system will store all muscle group information in the database.
+
+
+=====Workout Requirments=====
+* The system will allow the user to start new workout.
+* The system will allow the user to view their current workout.
+* The system will allow the user to view their workout history.
+* The system will allow the user to view their workout statistics.
+* The system will allow the user to view their workout progress.
+* The system will show video examples of each exercise.
+* The system will supply written instructions for each exercise.
+* The system will strore the user's workout history in the database.
+
+
+=====statistics Requirments=====
+* The system will allow the user to view their workout statistics..
+* The system will allow the user to view their workout history.
+* The system will show the user his maximum weight during the use of the application.
+* The system will show the user his minimum weight during the use of the application.
+* The system will show the user his average weight during the use of the application.
+* The system will count the number of workouts the user has done.
+* The system will show various graph with statistics information.
+* The system will calcuate the user's BMI.
+* The system will choose the right workout for the user based on his workout history.
+* The system will calcuate for each workout average weight change per day
+* The system will count the number of days in each workout.
+* The system will calcuate the weight need to gain / lose to reach valid BMI.
