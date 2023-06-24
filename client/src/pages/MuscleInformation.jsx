4072957e9ad03885423e3afa73b0c2fa5691afa0
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2023-06-24 22:13:51 +0300

    fix twice use effect call to server

--- client/src/pages/MuscleInformation.jsx
+++ client/src/pages/MuscleInformation.jsx
@@ -15,7 +15,6 @@ const MuscleInformation = () => {
   const [error, setError] = useState(true);
   const [musclesNames, setMusclesNames] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [flag, setFlag] = useState(false);
   const [dataVals, setDataVals] = useState({
     link: [],
     topic: [],
@@ -23,7 +22,6 @@ const MuscleInformation = () => {
     generalInformation: "",
   });
 
-  // get all muscles names from DB on first render
   useEffect(() => {
     const getDataMusclesNamesFromDB = async () => {
       await getMusclesNames();
@@ -32,68 +30,53 @@ const MuscleInformation = () => {
     getDataMusclesNamesFromDB();
   }, []);
 
-  // get all muscles information from DB
   useEffect(() => {
-    const getData = async () => {
-      await fetchmuscleInformation();
+    const fetchMuscleInformation = async () => {
+      const response = await getTrainingProgramas(muscle);
+      if (response === false) {
+        setError(true);
+      } else {
+        const updatedData = {
+          link: response.musclesInformation.topics.map((info) => info.link),
+          topic: response.musclesInformation.topics.map((info) => info.topic),
+          information: response.musclesInformation.topics.map(
+            (info) => info.information
+          ),
+          generalInformation: response.musclesInformation.generalInformation,
+        };
+        setDataVals(updatedData);
+        setError(false);
+      }
     };
-    getData();
-    // eslint-disable-next-line
+
+    if (muscle !== "") {
+      fetchMuscleInformation();
+    }
   }, [muscle]);
 
-  // get all muscles names from DB and set it to musclesNames
   const getMusclesNames = async () => {
     const response = await getTrainingProgramasName();
     if (response === false) {
-      setLoading(false);
-      return;
+      setError(true);
     } else {
       setError(false);
       setMusclesNames(response);
     }
   };
 
-  // get all muscles information from DB and set it to data
-  const fetchmuscleInformation = async () => {
-    const response = await getTrainingProgramas(muscle);
-    if (response === false) {
-      setError(true);
-      return;
-    } else {
-      setAllData(response);
-    }
-  };
-
-  // set all data to dataVals
-  const setAllData = async (data) => {
-    if (flag === true) {
-      const updatedData = {
-        ...dataVals,
-        link: data.musclesInformation.topics.map((info) => info.link),
-        topic: data.musclesInformation.topics.map((info) => info.topic),
-        information: data.musclesInformation.topics.map(
-          (info) => info.information
-        ),
-        generalInformation: data.musclesInformation.generalInformation,
-      };
-      setDataVals(updatedData);
-    }
-  };
-  // handle muscle change ant flag to true to update dataVals with new data
   const handleMuscleChange = (option) => {
     const selectedMuscle = musclesNames.find((muscle) => muscle === option);
     setMuscle(selectedMuscle);
-    setFlag(true);
   };
 
-  // if error return error page
   if (error && !loading) {
     return <ErrorPage toRemove={true} />;
   }
-  // if loading the data return loading page
+
   if (loading && !error) {
     return <ErrorPage toRemove={false} />;
   }
+
   if (!loading && !error) {
     return (
       <MainLayout>
@@ -120,4 +103,5 @@ const MuscleInformation = () => {
     );
   }
 };
+
 export default MuscleInformation;
