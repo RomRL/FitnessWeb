981d5a179a7e1b33e0ad75899d960d3ba2472aac
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2023-06-24 20:53:00 +0300

    before fix

--- client/src/controller/requests.js
+++ client/src/controller/requests.js
@@ -1,8 +1,8 @@
 //This page will include all the requests to the backend
 import axios from "axios";
 const format = {
-  baseUrl: "http://localhost:5000",
-  //baseUrl: "https://fitness-api-974s.onrender.com",
+  // baseUrl: "http://localhost:5000",
+  baseUrl: "https://fitness-api-974s.onrender.com",
 
 };
 
